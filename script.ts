b629f1219c49711d0c0456fa6196545efa794ee1
bebzbzbz <balzerbeatrice@gmail.com>
2024-12-29 12:20:36 +0100

    Rechner styled

--- script.ts
+++ script.ts
@@ -1,3 +1,7 @@
+const formDiv = document.querySelector("#form-div") as HTMLDivElement;
+const resultDiv = document.querySelector("#result-div") as HTMLDivElement;
+const result  = document.querySelector("#result") as HTMLDivElement;
+
 const btn = document.querySelector("input[type=submit]")
 btn?.addEventListener("click", tipsCalc)
 
@@ -9,18 +13,23 @@ function tipsCalc() {
     const total = Number((document.querySelector("#total") as HTMLInputElement).value);
     const people = Number((document.querySelector("#people") as HTMLInputElement).value);
     const service = Number((document.querySelector("#service") as HTMLSelectElement).value);
-    const result  = document.querySelector(".result") as HTMLDivElement;
     
-    result.innerHTML = `<p>Das <b>Trinkgeld</b> beträgt: ${total * service}€</p> 
-    <p>Die <b>Gesamtsumme</b> ist: ${total + (total * service)}€</p>
-    <p>Der Preis <b>pro Person</b> liegt bei: ${(total + (total * service))/people}€</p>
-    <a class="btn">Neu berechnen</a>`;
-    result.style.height = "100%";
+    result.innerHTML = `<p>Das <b>Trinkgeld</b> beträgt: ${(total * service).toFixed(2)}€</p> 
+    <p>Die <b>Gesamtsumme</b> ist: ${(total + (total * service)).toFixed(2)}€</p>
+    <p>Der Preis <b>pro Person</b> liegt bei: ${((total + (total * service))/people).toFixed(2)}€</p>`;
 
     // Slides to result
 
-    const form = document.querySelector("form") as HTMLFormElement;
-    form.style.height = "0";
-    form.style.overflow = "hidden";
+    formDiv.classList.toggle("hide");
 
+    resultDiv.classList.toggle("show");
+}
+
+const backBtn = document.querySelector("#back") as HTMLAnchorElement;
+backBtn.addEventListener("click", goBack)
+
+function goBack() {
+    formDiv.classList.toggle("hide");
+
+    resultDiv.classList.toggle("show");
 }
